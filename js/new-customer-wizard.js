a62a5a861b502d46c96d3505318c78269d793bd1
DavidBurn1981 <consultancyfirst@yahoo.com>
2026-09-11 15:31:48 +0100

    Add files via upload

--- js/new-customer-wizard.js
+++ js/new-customer-wizard.js
@@ -455,7 +455,7 @@ async function wizRecordSession(){
   }
   let age=ageFromDob(c.dob);
   if(hybrid&&age<18)return alert('CUSTOMER IS BELOW 18 AND IS NOT ALLOWED TO USE UV.');
-  if(hybrid&&age<25&&!c.idChecked)return alert('NO ID HAS BEEN CHECKED FOR THIS CUSTOMER. CHECK CUSTOMER ID BEFORE UV USE.');
+  if(hybrid&&age<25&&!c.idChecked)alert('Customer has not had ID checked and is under 25. This is not a blocker, just a note to user. Close to continue');
   if(hybrid&&!c.uvAllowed)return alert('This Customer can not use UV. Please check their Customer record to see why.');
   if(accountMin>c.minutesLeft){err.textContent=`Customer has ${c.minutesLeft} minutes left but this session requires ${accountMin} minutes from account.`;err.style.display='block';return}
   try{
